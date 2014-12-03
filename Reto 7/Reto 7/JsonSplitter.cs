ec90b1a24fdf00ac28f669241be7a896c9fb06e9
rsciriano <rsciriano@gmail.com>
2014-12-03 07:28:55 +0100

    Reto 7

---
+++ Reto 7/Reto 7/JsonSplitter.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Reto7
+{
+    public class JsonSplitter
+    {
+
+        /*
+         * Método mas rápido usando directamente la clase JArray de la librería http://james.newtonking.com/json
+         */ 
+        public static Tuple<string, string> SplitShowsByGenre(string trendingShowsJson, string p)
+        {
+            JArray a = new JArray(); JArray b = new JArray();
+            JArray list = JArray.Parse(trendingShowsJson);
+
+            list.ToList().ForEach(v => {
+                if (v["genres"].Values<string>().Contains(p))
+                    a.Add(v);
+                else
+                    b.Add(v);
+            });
+
+            return new Tuple<string, string>(
+                a.ToString(Formatting.None),
+                b.ToString(Formatting.None)
+            );
+        }
+
+        /*
+         * Método mas compacto para procesar JSON pero mas lento
+         * ademas necesita las clases tipadas que he generado con http://json2csharp.com/
+         */
+        public static Tuple<string, string> SplitShowsByGenre2(string trendingShowsJson, string p)
+        {
+            var list = JsonConvert.DeserializeObject<Film[]>(trendingShowsJson);
+
+            return new Tuple<string, string>(
+                JsonConvert.SerializeObject(list.Where(f => f.genres.Contains(p))),
+                JsonConvert.SerializeObject(list.Where(f => !f.genres.Contains(p)))
+            );
+        }
+
+
+        #region Otras variantes
+        public static Tuple<string, string> SplitShowsByGenre3(string trendingShowsJson, string p)
+        {
+            JArray list = JArray.Parse(trendingShowsJson);
+
+            JArray a = new JArray();
+            JArray b = new JArray();
+            list.Where(f => f["genres"].Values<string>().Contains(p)).ToList().ForEach(v =>  a.Add(v));
+            list.Where(f => !f["genres"].Values<string>().Contains(p)).ToList().ForEach(v => b.Add(v));
+
+            return new Tuple<string, string>(
+                a.ToString(Formatting.None),
+                b.ToString(Formatting.None)
+            );
+        }
+
+        public static Tuple<string, string> SplitShowsByGenre4(string trendingShowsJson, string p)
+        {
+            JsonTextReader tr = new JsonTextReader(new StringReader(trendingShowsJson));
+            JsonSerializer seria = new JsonSerializer();
+            JArray list = (JArray)seria.Deserialize(tr);
+
+            JArray a = new JArray();
+            JArray b = new JArray();
+            list.Where(f => f["genres"].Values<string>().Contains(p)).All(p1 => { a.Add(p1); return true; });
+            list.Where(f => !f["genres"].Values<string>().Contains(p)).All(p2 => { b.Add(p2); return true; });
+
+            return new Tuple<string, string>(
+                a.ToString(Formatting.None),
+                b.ToString(Formatting.None)
+            );
+        }
+
+        public static Tuple<string, string> SplitShowsByGenre5(string trendingShowsJson, string p)
+        {
+            JsonTextReader tr = new JsonTextReader(new StringReader(trendingShowsJson));
+            JsonSerializer seria = new JsonSerializer();
+            var list = seria.Deserialize<Film[]>(tr);
+
+            return new Tuple<string, string>(
+                JsonConvert.SerializeObject(list.Where(f => f.genres.Contains(p))),
+                JsonConvert.SerializeObject(list.Where(f => !f.genres.Contains(p)))
+            );
+        }
+        #endregion
+    }
+}
---
+++ Reto 7/Reto 7/Model.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reto7
+{
+    public class Images
+    {
+        public string poster { get; set; }
+        public string fanart { get; set; }
+        public string banner { get; set; }
+    }
+
+    public class Ratings
+    {
+        public int percentage { get; set; }
+        public int votes { get; set; }
+        public int loved { get; set; }
+        public int hated { get; set; }
+    }
+
+    public class Film
+    {
+        public string title { get; set; }
+        public int year { get; set; }
+        public string url { get; set; }
+        public int first_aired { get; set; }
+        public string country { get; set; }
+        public string overview { get; set; }
+        public int runtime { get; set; }
+        public string status { get; set; }
+        public string network { get; set; }
+        public string air_day { get; set; }
+        public string air_time { get; set; }
+        public string certification { get; set; }
+        public string imdb_id { get; set; }
+        public string tvdb_id { get; set; }
+        public string tvrage_id { get; set; }
+        public string poster { get; set; }
+        public Images images { get; set; }
+        public int watchers { get; set; }
+        public Ratings ratings { get; set; }
+        public List<string> genres { get; set; }
+    }
+}
