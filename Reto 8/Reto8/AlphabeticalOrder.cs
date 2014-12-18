cb186eca5454e8235f53470c2d5056fd00a36809
rsciriano <rsciriano@gmail.com>
2014-12-18 22:17:06 +0100

    Reto 8: Solución refactorizada con un solo comparador

--- Reto 8/Reto8/AlphabeticalOrder.cs
+++ Reto 8/Reto8/AlphabeticalOrder.cs
@@ -11,7 +11,7 @@ namespace Reto8
         /// <summary>
         /// Comparador de la primera fase: comparar solo los primeros caracteres de la cadena mas corta 
         /// </summary>
-        public class LexicographicalStringComparer_Phase1 : IComparer<string>
+        public class LexicographicalStringComparer : IComparer<string>
         {
             public int Compare(string x, string y)
             {
@@ -19,45 +19,27 @@ namespace Reto8
                     throw new ArgumentNullException();
 
                 int min = Math.Min(x.Length, y.Length);                
+                
+                // Comparación estandar de los primeros caracteres
                 int r = 0;
                 for (int i = 0; i < min; i++)
                 {
                     r = x[i].CompareTo(y[i]);
                     if (r != 0)
-                        break;
+                        return r;
                 }
-                return r;
+
+                // Comparación contatenando las dos cadenas cuando coinciden los primeros caracteres
+                return (x + y).CompareTo(y + x); 
             }
         }
-
-        /// <summary>
-        /// Comparador segunda fase: comtarar cadenas concatenadas
-        /// </summary>
-        public class LexicographicalStringComparer_Phase2 : IComparer<string>
-        {
-            public int Compare(string x, string y)
-            {
-                if (x == null || y == null)
-                    throw new ArgumentNullException();
-
-                return (x + y).CompareTo(y + x);
-            }
-        }
-
 
         public static string GetShortestConcatString(string str)
         {
             if (str == null)
                 throw new ArgumentNullException();
 
-            string[] list = str.Split(' ');
-
-            // Ordenación en dos fases
-            var sortedList = list
-                .OrderBy(s => s, new LexicographicalStringComparer_Phase1())
-                .ThenBy(s => s, new LexicographicalStringComparer_Phase2());
-
-            return string.Join("", sortedList);
+            return string.Join("", str.Split(' ').OrderBy(s => s, new LexicographicalStringComparer()));
         }
         public static IEnumerable<string> GetShortestConcatString(params string[] strs)
         {
